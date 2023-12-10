91c0f9c299be899005a55167cb93cbcaf834564c
JaydeepGedam <jaydeep.gedam444@gmail.com>
2023-12-10 16:15:04 +0530

    changed case

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = ({reference}) => {
     <div className='flex'>
       <div className='w-[50%] px-[90px] py-[70px] h-[calc(744px-83px)]'>
         <motion.h1 drag dragConstraints={reference} className='uppercase text-8xl font-extrabold'>your feet deserve the best</motion.h1>
-        <motion.p drag dragConstraints={reference} className='uppercase mt-5 text-zinc-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil laudantium voluptas tempora tenetur rem explicabo nostrum, odit quibusdam consequuntur esse obcaecati cumque, inventore alias. Aliquam nam ducimus consequuntur porro neque!</motion.p>
+        <motion.p drag dragConstraints={reference} className='capitalize mt-5 text-zinc-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil laudantium voluptas tempora tenetur rem explicabo nostrum, odit quibusdam consequuntur esse obcaecati cumque, inventore alias. Aliquam nam ducimus consequuntur porro neque!</motion.p>
         <div className='mt-5 flex gap-4'>
             <motion.button drag dragConstraints={reference} className='capitalize font-bold bg-red-600 px-4 py-2'>shop now</motion.button>
             <motion.button drag dragConstraints={reference} className='capitalize font-bold transparent border px-4 py-2'>category</motion.button>
